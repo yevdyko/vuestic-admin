da416f51616d1f95ccfa14120c7bcb28bf92e313
Yev Dyko <yevdyko@gmail.com>
2018-03-21 00:33:05 +0100

    feat(color-picker): add vuestic-color-picker component

---
+++ src/vuestic-theme/vuestic-components/vuestic-color-picker/VuesticColorPicker.vue
@@ -0,0 +1,38 @@
+<template>
+</template>
+
+<script>
+  import SimpleInline from './color-picker-types/SimpleInline.vue'
+
+  export default {
+    name: 'vuestic-color-picker',
+
+    components: {
+      SimpleInline
+    },
+
+    props: {
+
+    },
+
+    data () {
+    },
+
+    computed: {
+      colorObject () {
+        return {
+          hex: this.value
+        }
+      }
+    },
+
+    methods: {
+      change (value) {
+        this.$emit('input', value.hex)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/vuestic-theme/vuestic-components/vuestic-color-picker/color-picker-types/SimpleInline.vue
@@ -0,0 +1,46 @@
+<template>
+  <compact-picker
+    :value="colorObject"
+    @input="change"
+  >
+  </compact-picker>
+</template>
+
+<script>
+  import { Compact as CompactPicker } from 'vue-color'
+
+  export default {
+    name: 'simple-inline',
+
+    components: {
+      CompactPicker
+    },
+
+    props: {
+      value: {
+        type: String,
+        required: true
+      }
+    },
+
+    data () {
+    },
+
+    computed: {
+      colorObject () {
+        return {
+          hex: this.value
+        }
+      }
+    },
+
+    methods: {
+      change (value) {
+        this.$emit('input', value.hex)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
